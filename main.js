817956501696582f109271b582150f5ba7aa459f
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-11-03 19:10:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
            
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
            
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var names = ["Álbum de Família","Rodrigo Silva", "Diego Silva", "Roberto Silva", "Aline Silva", "Sonia Silva"];
           
 var i = 0;
 function update()
