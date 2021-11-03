d8247796a32bbd0ffbed3cf3753cb6c3ed6fad0f
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-11-03 16:53:26 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-
+<script>
             var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg","https://i.postimg.cc/L6HcYQM0/grand-father.png", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
             var names = ["Álbum de Família","Rodrigo Silva", "Diego Silva", "Roberto Silva", "Aline Silva", "Sonia Silva"];
             var i = 0;
@@ -15,3 +15,4 @@
                 document.getElementById("familyMemberImage").src = updatedImage;
                 document.getElementById("familyMemberName").innerHTML = updatedName;
             }
+</script>
