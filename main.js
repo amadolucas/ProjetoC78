7836b1c12473428ac0b3e81838d785ab371e3a99
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-11-03 17:05:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ var i = 0;
 function update()
 {
      i++;
-     var numbersOfFamilyMemberInArray = 5
-     if(i > numbersOfFamilyMemberInArray )
+     var numbers_of_family_member_in_array = 5
+     if(i > numbers_of_family_member_in_array)
      {
         i = 0;
      }
      var updatedImage = images[i];
      var updatedName  = names[i];
-     document.getElementById("familyMemberImage").src = updatedImage;
-     document.getElementById("familyMemberName").innerHTML = updatedName; 
+     document.getElementById("family_member_image").src = updatedImage;
+     document.getElementById("family_member_name").innerHTML = updatedName; 
 }
